79adc7b5ac9096d3bf57442fb598eb45d220b7d8
Pete R Jemian <prjemian@gmail.com>
2023-09-28 00:15:07 -0500

    updated test

--- try_libhkl.py
+++ try_libhkl.py
@@ -2,12 +2,13 @@ import gi
 
 gi.require_version("Hkl", "5.0")
 from gi.repository import Hkl as libhkl
-from gi.repository import GLib  # noqa: F401
 
 # access some content
 diffractometer_types = libhkl.factories()
 assert isinstance(diffractometer_types, dict)
 assert 5 < len(diffractometer_types) < 20
+for i, dt in enumerate(sorted(diffractometer_types), start=1):
+    print(f"{i} {dt}  {diffractometer_types[dt]}")
 
 UserUnits = libhkl.UnitEnum.USER
 assert UserUnits is not None

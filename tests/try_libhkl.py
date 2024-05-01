55075eea969ccbb9c717d58df9c3c2e6f0fe36ae
Pete R Jemian <prjemian@gmail.com>
2024-04-30 23:31:32 -0500

    TST #16 upgrade testing tools

--- tests/try_libhkl.py
+++ tests/try_libhkl.py
@@ -2,20 +2,20 @@
 
 import gi  # gobject-introspection, to access libhkl
 gi.require_version("Hkl", "5.0")
-from gi.repository import Hkl as libhkl
+from gi.repository import Hkl as libhkl  # noqa: E402
 
 # access some content
+assert "VERSION" in dir(libhkl)
+print(f"{libhkl.VERSION=}")
+
 diffractometer_types = libhkl.factories()
 assert isinstance(diffractometer_types, dict)
-assert 5 < len(diffractometer_types) < 30
+assert 5 < len(diffractometer_types) < 50
 for i, dt in enumerate(sorted(diffractometer_types), start=1):
-    print(f"{i} {dt}  {diffractometer_types[dt]}")
+    print(f"{i}\t{dt}")
 
 UserUnits = libhkl.UnitEnum.USER
 assert UserUnits is not None
 
 DefaultUnits = libhkl.UnitEnum.DEFAULT
 assert DefaultUnits is not None
-
-assert "VERSION" in dir(libhkl)
-assert libhkl.VERSION.startswith("v5.0.0")

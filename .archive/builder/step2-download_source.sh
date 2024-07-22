172a4a024bb26b952e8d44c0effc1314e85f7250
Pete R Jemian <prjemian@gmail.com>
2024-07-22 00:46:21 -0500

    DOC #22

--- .archive/builder/step2-download_source.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e pipefail
-
-echo "Clone repo from: ${HKL_REPO_URL}"
-git clone "${HKL_REPO_URL}"
-
-# Additional libraries need to compile libhkl.
-apt-get install -y libcglm-dev ghc # libinih-dev

5c7367d009f98e741ff32844d1b469a66251ce40
Pete R Jemian <prjemian@users.noreply.github.com>
2024-07-23 16:38:20 -0500

    Update tests/resources/install-libhkl.sh
    Co-authored-by: Padraic Shafer <76011594+padraic-shafer@users.noreply.github.com>

--- tests/resources/install-libhkl.sh
+++ tests/resources/install-libhkl.sh
@@ -6,5 +6,5 @@ echo CONDA_DIR="${CONDA_DIR}"
 echo CONDA_PREFIX="${CONDA_PREFIX}"
 
 source $CONDA_DIR/etc/profile.d/conda.sh
-tar xzf /home/libhkl.tar.gz -C $CONDA_PREFIX
+tar -xzf /home/libhkl.tar.gz -C $CONDA_PREFIX
 echo "VERSION: $(grep VERSION $CONDA_PREFIX/include/hkl-5/hkl.h)"
